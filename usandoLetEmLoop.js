46d9dc202444b239b147a7ab2e4197735e559525
Igor Montezuma <igorminranda17@gmail.com>
2022-01-03 21:31:29 -0300

    Estudo de Fundamentos em JS.

---
+++ usandoLetEmLoop.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("i =", i);
+
+//Já diferente do var, com o let, a variável fica disponível apenas dentro do escopo, e causa erro se for chamada fora do mesmo.
---
+++ usandoVarEmLoop1.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("i =", i);
+
+//Quando a variavel é chamada fora do laço, ela é retornada com o valor que fez ela sair da condição declarada.
+//No caso foi apresentado o valor 11, pois ele passou da condição que foi imposta no laço.
