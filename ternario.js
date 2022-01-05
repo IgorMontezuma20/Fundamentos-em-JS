f78db3bfc765cb7330743542c0dfd62b0a580553
Igor Montezuma <igorminranda17@gmail.com>
2022-01-04 21:24:53 -0300

    Tratamento de Erros.

--- ternario.js
+++ ternario.js
@@ -1,4 +1,4 @@
-const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' // ? = verdadeiro  : = falso.
+const resultado = (nota) => (nota >= 7 ? "Aprovado" : "Reprovado"); // ? = verdadeiro  : = falso.
 
-console.log(resultado(7.1))
-console.log(resultado(6.7))
+console.log(resultado(7.1));
+console.log(resultado(6.7));
