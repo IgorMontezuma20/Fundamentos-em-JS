1c46be18120d285c27989aac5fa2c2e3e4b6b72e
Igor Montezuma <igorminranda17@gmail.com>
2022-01-06 19:43:14 -0300

    Estrutura For.

---
+++ fo1.js
@@ -0,0 +1,16 @@
+//let contador = 1
+//while(contador <= 10){
+//    console.log(`Contador = ${contador}`)
+//    contador++
+//}
+
+for(let c = 1; c <= 10; c ++){
+    console.log(`Contador = ${c}`)
+}
+
+console.log('\n')
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.8]
+for(let i = 0; i< notas.length; i++){
+    console.log(`Nota = ${notas[i]}`)
+}
